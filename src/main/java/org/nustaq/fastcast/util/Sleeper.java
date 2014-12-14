2213af33a2c19de661942ca223e8be87e2330a42
RuedigerMoeller <moru0011@gmail.com>
2014-12-14 19:47:01 +0100

    move to apache 2 license

--- src/main/java/org/nustaq/fastcast/util/Sleeper.java
+++ src/main/java/org/nustaq/fastcast/util/Sleeper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Ruediger Moeller.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.nustaq.fastcast.util;
 
 /**
@@ -5,21 +20,15 @@ package org.nustaq.fastcast.util;
  * User: moelrue
  * Date: 8/12/13
  * Time: 2:28 PM
- * To change this template use File | Settings | File Templates.
  */
 public class Sleeper {
 
-    int sleepcount = 0;
     public void sleepMicros(int micro) {
         if ( micro <= 0 )
             return;
-        sleepcount+=micro;
-        try {
-            if ( sleepcount >= 1000 ) {
-                Thread.sleep(sleepcount/1000);
-                sleepcount%=1000;
-            }
-        } catch (InterruptedException e) {
+        long now = System.nanoTime();
+        while( System.nanoTime()-now < micro*1000 ) {
+            // spin
         }
     }
 
